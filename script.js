83043cd2dcd6717d7ef2e3e3ad1e9765df7f8a03
Manuel Schmalstieg <git.code@ms-studio.net>
2020-06-24 01:22:07 +0200

    optimisations du script: meilleure gestion du retina

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 var canvas = document.getElementById("myCanvas");
-canvas.width = 1000;
-canvas.height = 1000;
 var ctx = canvas.getContext("2d");
 
-var r1 = getRandom(0, 1000);
-var r2 = getRandom(0, 1000);
-var r3 = getRandom(0, 1000);
-var r4 = getRandom(0, 1000);
+// get CSS pixel size, prepare for retina
+// var canvasRect = canvas.getBoundingClientRect();
+var cSize = canvas.offsetWidth * 2;
+canvas.width = cSize;
+canvas.height = cSize;
+var canvasX = canvas.offsetLeft;
+var canvasY = canvas.offsetTop;
+
+var r1 = getRandom(0, cSize);
+var r2 = getRandom(0, cSize);
+var r3 = getRandom(0, cSize);
+var r4 = getRandom(0, cSize);
 var myTimer;
 
 function randomize() {
-  r1 = getRandom(0, 1000);
-  r2 = getRandom(0, 1000);
+  r1 = getRandom(0, cSize);
+  r2 = getRandom(0, cSize);
   // r3 = getRandom(0, 1000);
   // r4 = getRandom(0, 1000);
 }
 
 // le code pour dessiner
 function draw() {
+  //ctx.clearRect(0, 0, canvas.width, canvas.height);
   randomize();
   ctx.beginPath();
   ctx.moveTo(r1, r2);
   ctx.lineTo(r3, r4);
+  ctx.lineWidth = 2;
   ctx.stroke();
+
 }
 
 // Intercepter le clic
 document
   .getElementById("myCanvas")
   .addEventListener("mousedown", function (event) {
-    myTimer = setInterval(draw, 100);
-    // r3 = event.clientX;
-    // r4 = event.clientY;
+    myTimer = setInterval(draw, 200);
+    // position du clic
+    r3 = (event.clientX - canvasX) * 2;
+    r4 = (event.clientY - canvasY) * 2;
   });
 
 document.getElementById("myCanvas").addEventListener("mouseup", function () {
